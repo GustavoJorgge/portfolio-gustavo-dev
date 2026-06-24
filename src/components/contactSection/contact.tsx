9401c656715f3eba59999289f21c5b78bf9c0565
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2026-06-23 21:47:58 -0300

    refact: contact

--- src/components/contactSection/contact.tsx
+++ src/components/contactSection/contact.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { MessageCircleCode } from "lucide-react";
-import { Button } from "./ui/button";
+import { Button } from "../ui/button";
 
 export function Contact() {
   return (
